b582ec857a6b78b9f2a3d317e04e49f4bff3208a
EdwinBerrio <bmedwin.20@hotmail.com>
2016-05-25 18:41:22 -0500

    primera actualizacion

---
+++ LayerData/LayerDataControl2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayerData
+{
+    class LayerDataControl2
+    {
+        //string de conexion
+        public string strconn = @"tcp:lv0t2edij8.database.windows.net,1433;Database=BDcontrol2;User ID=BDcontrol2@lv0t2edij8;Password={aquí_va_la_contraseña};Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+
+
+
+    }
+}
