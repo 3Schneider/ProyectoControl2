df77b8961f481012d2a59d050c5be1bf1acf238d
3Schneider <esneidervr@misena.edu.co>
2016-05-27 19:43:09 -0500

    aspx ingresar

--- LayerData/LayerDataControl2.cs
+++ LayerData/LayerDataControl2.cs
@@ -10,8 +10,13 @@ namespace LayerData
     {
         //string de conexion
         public string strconn = @"tcp:lv0t2edij8.database.windows.net,1433;Database=BDcontrol2;User ID=BDcontrol2@lv0t2edij8;Password={aquí_va_la_contraseña};Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+        //metodo constructor
+        public LayerDataControl2() { }
 
+        public int InsertarProduccion()
+        {
 
+        }
 
     }
 }
